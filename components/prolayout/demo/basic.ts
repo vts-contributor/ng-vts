bf3d976e7a7de6e20c02517d63d690d2b96a75b1
fog9999 <nguyenducthang20081999@gmail.com>
2023-03-29 15:58:41 +0700

    bring in tree view, tree-select, modal

--- components/prolayout/demo/basic.ts
+++ components/prolayout/demo/basic.ts
@@ -22,7 +22,7 @@ export class VtsDemoProlayoutBasicComponent {
       children: [
         {
           title: 'Child 1.1',
-          children: [{ title: 'Child 1.1.1' }, { title: 'Child 1.1.2', isSelected: true, url: "/" }],
+          children: [{ title: 'Child 1.1.1', url: "/components/prolayout/en" }, { title: 'Child 1.1.2', isSelected: true, url: "/" }],
           isOpen: true
         },
         { title: 'Child 1.2' }
